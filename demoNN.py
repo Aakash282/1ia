1eb6a7e5cdc8cfea248e7abb21326b497b2259c2
Aakash Indurkhya <aindurkh@caltech.edu>
2015-08-16 00:53:18 -0700

    WIP: neural network

---
+++ demoNN.py
@@ -0,0 +1,20 @@
+# NN demo
+from NN import bpnn
+
+if __name__ == "__main__":
+    # Teach network XOR function
+    pat = [
+        [[0,0], [0]],
+        [[0,1], [1]],
+        [[1,0], [1]],
+        [[1,1], [0]]
+    ]
+
+    # create a network with two input, two hidden, and one output nodes
+    n = bpnn.BPNN([2, 2, 1])
+    # train it with some patterns
+    n.train(pat, L=.2)
+    # test it
+    
+    for p in pat: 
+        print str(p[0]) + " -> %f" % (n.predict(p[0]))
