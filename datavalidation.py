1abeffee69cdc642684844e71031f657bad895a2
edwardgarza <edward.garza0@gmail.com>
2015-08-16 09:38:31 -0400

    added function to sort data by team and look at home field adv

--- datavalidation.py
+++ datavalidation.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import csv
+# May want to make table a global variable?
+
 
 def parse_data():
     '''processes the data.  Change the file location'''
@@ -36,6 +38,7 @@ def import_table():
     return pd.read_csv('/home/jamal/FSA/data/2001-2008data.csv')
     
 def validate():
+    '''basic sanity checks on the numerical parts of the data'''
     table = import_table()
     TOP = {}
     # These are inequalities
@@ -56,17 +59,76 @@ def validate():
                          int(x.split(':')[1]) for x in table['ht TOP']])
     away_TOP = np.array([int(x.split(':')[0])*60 + \
                          int(x.split(':')[1]) for x in table['at TOP']])
-    assert(len(set(home_TOP + away_TOP >= 3595) == 1))
+    assert(len(set(home_TOP + away_TOP >= 3595)) == 1 )
     
 def get_teams():
+    '''returns a dictionary of teams separated by their location.  Ex:
+    get_teams()['Dallas Cowboys home'] would return the cowboys' home game
+    information'''
     table = import_table()
-    #table.describe()
+    #table.describe() useful to use later
     teams = {}
     team_keys = list(table['home team'].values) + list(table['away team'].values)
     for name in team_keys:        
-        teams[name] = table[table['home team'] == name]
-        teams[name].append(table[table['away team'] == name])
+        for location in ['home', 'away' ]:
+            teams[name + " " + location] = table[table[location + ' team'] == name]
+    return teams, table
+
+def home_away_differences():
+    ''' Finds the differences between teams and their opponents based on
+    location.  Returns a dictionary of numpy arrays.  Ex. use:
+    
+    a = home_away_differences()
+    plt.hist(a['Dallas Cowboys home score'], 20)
+    
+    which would plot a hist of how many more points the Cowboys have vs their
+    opponent when the Cowboys are playing at home.  
+    a['Dallas Cowboys away score'] would be their stats when they are away
+    '''
+    all_teams, table = get_teams()
+    team_keys = get_team_keys()
+    columns = list(table.keys())
+    # ghetto, hardcoded filtering method :(
+    columns = [x[3:] for x in columns if ('at' in x)]    
+    columns.remove('endance')
+    columns.remove('TOP')
+    stat_diff = {}
+    for team in team_keys:
+        for stat in columns:
+            for location in [(' home', 'ht ', 'at '), (' away', 'at ', 'ht ')]:
+                stat_diff[team + location[0] + ' ' + stat] = \
+                    np.array(all_teams[team + location[0]][location[1] + stat]) - \
+                    np.array(all_teams[team + location[0]][location[2] + stat])
+    return stat_diff
+
+def get_team_keys():
+    table = import_table()
+    teams = list(set(list(table['home team'].values) + list(table['away team'].values)))
+    # filters out spurious data.  Should change in the future
+    teams = [x for x in teams if len(x) > 4]
     return teams
+
+def combine_home_adv(stat):
+    '''Finds the difference between a given stat for all teams for home field
+    advantage.  Returns a numpy array.  Ex usage would be
+    
+    diff = combine_home_adv('score')
+    diff.mean() -> the average score difference'''
+    
+    stat_diffs = home_away_differences()
+    team_keys = get_team_keys()
+    diff = []
+    for team in team_keys:
+        if len(stat_diffs[team + ' home ' + stat]) != 0 and \
+           len(stat_diffs[team + ' away ' + stat]) != 0:
+                diff.append(stat_diffs[team + ' home ' + stat].mean() -\
+                            stat_diffs[team + ' away ' + stat].mean())
+    # I think I am double counting here.  Not sure, but dividing by two only 
+    # fixes the median problems
+    return np.array(diff)/2.0
+        
+    
+    
     
     
     
