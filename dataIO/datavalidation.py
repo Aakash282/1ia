99e75e0d2efb45dfa2872af98216408afce61f3d
edwardgarza <edward.garza0@gmail.com>
2015-08-17 20:56:56 -0400

    moved to tableFns

--- dataIO/datavalidation.py
+++ dataIO/datavalidation.py
@@ -52,17 +52,6 @@ def validate():
     away_TOP = np.array([int(x.split(':')[0])*60 + \
                          int(x.split(':')[1]) for x in table['at_TOP']])
     assert(len(set(home_TOP + away_TOP >= 3595)) == 1 )
-    
-def get_teams(table):
-    '''returns a dictionary of teams separated by their location.  Ex:
-    get_teams()['Dallas Cowboys home'] would return the cowboys' home game
-    information'''
-    teams = {}
-    team_keys = list(table['home_team'].values) + list(table['away_team'].values)
-    for name in team_keys:        
-        for location in ['home', 'away' ]:
-            teams[name + location] = table[table[location + '_team'] == name]
-    return teams
 
 
 def home_away_differences():
@@ -95,58 +84,7 @@ def home_away_differences():
                     np.array(all_teams[team + location[0]][location[2] + stat])
     return stat_diff
 
-def get_team_keys():
-    '''gets a list of all teams'''
-    table = load.getDataset()
-    teams = list(set(list(table['home_team'].values) + list(table['away_team'].values)))
-    teams = [x.strip() for x in teams]
-    return teams
-        
-def cum_stat(stat):
-    '''Gives a rough idea of the dsistribution of a stat for all games at
-    a glance.  Input is a string'''
-    table = load.getDataset()
-    return table.describe()[stat]
 
-def cum_array(stat):
-    '''Returns a pd series of a particular stat from the table. Input is 
-    a string'''
-    return load.getDataset()[stat]
-    
-def team_array():
-    '''Returns a dictionary of pd dataframe objects.  Filters out the data
-    to only include a specific team. Ex usage:
-    
-    df = team_array()
-    print df['Dallas Cowboys']
-    '''
-    table = load.getDataset()
-    teams = get_teams(table)
-    team_keys = get_team_keys()
-    combined = {}
-    for team in team_keys:
-        combined[team] = pd.merge(teams[team + '_home'], \
-                                  teams[team + '_away'], how = 'outer')
-    return combined
-
-
-def team_array_1(team):
-    '''exactly the same as team_array except for a specific team.  much cleaner
-    than the other function; may delete team_array.  ex:
-    team_array_1('Dallas Cowboys') == team_array()['Dallas Cowboys']
-    '''
-    table = load.getDataset()
-    return pd.merge(filter_table(table, 'home_team', team), \
-             filter_table(table, 'away_team', team), how = 'outer')
-
-def filter_table(table, column, entry):
-    '''filters the table to only include certain entries'''
-    return table[table[column] == entry]
-    
-def get_values(table, column):
-    '''returns all unique values for a given column'''
-    return set(table[column].values)
-    
     
     
     
