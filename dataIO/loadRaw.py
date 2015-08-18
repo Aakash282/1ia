1783dc854eba10e145ec58042f14f30b596a9950
edwardgarza <edward.garza0@gmail.com>
2015-08-17 20:42:38 -0400

    added in the week and year data into the table

--- dataIO/loadRaw.py
+++ dataIO/loadRaw.py
@@ -6,20 +6,24 @@ import os
 
 def parse():
     cwd = os.getcwd()
-    FSA = cwd.strip("1ia")
-    datadir = FSA + "data/"
+    FSA = cwd.strip("1ia/dataIO")
+    datadir = '/' + FSA + "/data/"
 
-    data = []
+    p = pd.read_csv(datadir + "headers.csv", sep = ";")
     for y in range(2001, 2015):
-        data.append(pd.DataFrame.from_csv(datadir + "features/features%d" % y, sep=";"))
-
-    data = pd.concat(data)
-    data = data.reset_index(drop=True)
-    data.to_csv(datadir + "NFL0114_TeamStats_raw.csv")
+        print y
+        lst_temp = pd.read_csv(datadir + "features/features%d" % y, sep=";")
+        for line in range(len(lst_temp['week'])):
+            lst_temp['week'][line] = str(lst_temp['week'][line]) + " %d" %y
+          
+        p = pd.merge(p, lst_temp, how = 'outer')
+    columns = list(p.keys())
+    columns[0] = 'week year'
+    p.columns = columns
+    p.to_csv(datadir + "NFL0114_TeamStats_raw.csv")
 
 def getDataset():
     cwd = os.getcwd()
-    FSA = cwd.strip("1ia")
-    datadir = FSA + "data/"
-
+    FSA = cwd.strip("1ia/dataIO")
+    datadir = '/' + FSA + "/data/"
     return pd.DataFrame.from_csv(datadir + "NFL0114_TeamStats_raw.csv")
