110ad4248f3de93680e1a2a37fa9e9a7a0ba0e16
edwardgarza <edward.garza0@gmail.com>
2015-09-02 21:34:44 -0400

    changed yearly data to dict instead of list

--- dataIO/loadData.py
+++ dataIO/loadData.py
@@ -1,28 +1,25 @@
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import loadRaw as load
-
+import re
 
 def loadYear(year):
-    ospath = load.getPath()
-    datadir = ospath + "/data/teamdatabyyear/"
-    y = year
-    yearDataPath = datadir + "teamdata%d/" % y
+    datadir = os.path.expanduser('~') + "/FSA/data/teamdatabyyear/"
+    yearDataPath = datadir + "teamdata%d/" % year
     teamFiles = os.listdir(yearDataPath)
-
     teams = [open(yearDataPath + team, 'r') for team in teamFiles]
-    teamData = [pd.DataFrame.from_csv(t) for t in  teams]
-    season = [team.sort(['week year'], ascending=[1]).reset_index(drop=True) for team in teamData]
+    season = {}
+    for t in teams:
+        a = re.search('/.*.csv', str(t))
+        #holy shit this line is bad. halp pls.  
+        team_name = a.group(0).split('/')[-1][:-4]
+        temp_stor = pd.DataFrame.from_csv(t)
+        season[team_name] = temp_stor.sort(['week year'], ascending=[1]).reset_index(drop=True)
     return season
 
 def getTeamData(year, team):
-    ospath = load.getPath()
-    path = ospath + '/data/teamdatabyyear/teamdata%d/%s.csv' %(year,team)
+    path = os.path.expanduser('~') + '/FSA/data/teamdatabyyear/teamdata%d/%s.csv' %(year,team)
     return pd.DataFrame.from_csv(path)
 
 def getYearData(year):
-    ospath = load.getPath()
-    path = ospath + '/data/NFLstatsbyyear/NFL0114_TeamStats_raw%d.csv' %year
+    path = os.path.expanduser('~') + '/FSA/data/NFLstatsbyyear/NFL0114_TeamStats_raw%d.csv' %year
     return pd.DataFrame.from_csv(path)
