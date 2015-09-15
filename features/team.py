787ffcb823cafd9818e19ba28b3c7e6b575576b1
Sarthak Sahu <ssahu@caltech.edu>
2015-09-14 18:23:02 -0700

    implementing record_vs_spread bug fix

--- features/team.py
+++ features/team.py
@@ -51,14 +51,14 @@ class team:
 						if w == 0:
 							record_vs_spread[w] = 1
 						else:
-							record_vs_spread[w] = (record_vs_spread[w - 1] + 1) / float(season[this_team]['week year'][w])
+							record_vs_spread[w] = (record_vs_spread[w - 1] * (w - 1) + 1) / float(season[this_team]['week year'][w])
 
 					# If this_team lost against the spread
 					else:
 						if w == 0:
 							record_vs_spread[w] = 0
 						else:
-							record_vs_spread[w] = record_vs_spread[w - 1] / float(season[this_team]['week year'][w])
+							record_vs_spread[w] = record_vs_spread[w - 1] * (w - 1) / float(season[this_team]['week year'][w])
 
 				# If spread uses opp_team
 				else:
@@ -68,14 +68,14 @@ class team:
 						if w == 0:
 							record_vs_spread[w] = 1
 						else:
-							record_vs_spread[w] = (record_vs_spread[w - 1] + 1) / float(season[this_team]['week year'][w])
+							record_vs_spread[w] = (record_vs_spread[w - 1] * (w - 1) + 1) / float(season[this_team]['week year'][w])
 					
 					# If this_team lost against the spread
 					else:
 						if w == 0:
 							record_vs_spread[w] = 0
 						else:
-							record_vs_spread[w] = record_vs_spread[w - 1] / float(season[this_team]['week year'][w])
+							record_vs_spread[w] = record_vs_spread[w - 1] * (w - 1) / float(season[this_team]['week year'][w])
 			
 			# If spread is Pick
 			else:
@@ -85,19 +85,19 @@ class team:
 					if w == 0:
 						record_vs_spread[w] = 1
 					else:
-						record_vs_spread[w] = (record_vs_spread[w - 1] + 1) / float(season[this_team]['week year'][w])
+						record_vs_spread[w] = (record_vs_spread[w - 1] * (w - 1) + 1) / float(season[this_team]['week year'][w])
 
 				# this_tem lost against the spread
 				else:
 					if w == 0:
 						record_vs_spread[w] = 0
 					else:
-						record_vs_spread[w] = record_vs_spread[w - 1] / float(season[this_team]['week year'][w])
+						record_vs_spread[w] = record_vs_spread[w - 1] * (w - 1) / float(season[this_team]['week year'][w])
 			
 			print year, w, season[this_team]['week year'][w], season[this_team]['team'][w], season[this_team]['opp_team'][w], season[this_team]['home_field?'][w], season[this_team]['score'][w], season[this_team]['opp_score'][w], season[this_team]['spread'][w]
 
 		print record_vs_spread
-		
+
 		season = np.matrix(self.season.values)[:-1]
 		features = np.empty_like(season[0:season.shape[0]-(n-1)])
 		
