37da476044d64bc511b546778d11ecd135b73915
John Co-Reyes <jdcoreyes@gmail.com>
2015-09-16 00:09:31 -0700

    Remove timer

--- features/features.py
+++ features/features.py
@@ -17,10 +17,9 @@ def get_feature_set(start, stop):
     # the -1 is because I like to use my computer while running things
     num_cores = min((stop - start) + 1, multiprocessing.cpu_count() - 1)
     # to ensure that at least one core is being used
-    num_cores = 3 #max(num_cores, 1)
+    num_cores = max(num_cores, 1)
 
     Parallel(n_jobs = num_cores)(delayed(feature_set)(x) for x in range(start, stop + 1))
-    #feature_set(2001)
     print 'Ellapsed time', default_timer() - start_time, 'Using %i core(s)' %num_cores
 
 def extractFeatures(team, week, year):
@@ -50,13 +49,13 @@ def feature_set(year):
         movingAvgLength = 3
         temp_features = temp_game.get_features(movingAvgLength)
 
-        if None in temp_features['away'].values() or \
-           None in temp_features['home'].values():
+        if None in temp_features['away'].values() or None in temp_features['home'].values():
             continue
         score_diff = temp_features['home']['score'] - temp_features['away']['score']
-        feature_lst = temp_features['away'].values() + temp_features['home'].values() + [score_diff] + temp_features['game'].values() + [week_year]
+        feature_lst = temp_features['away'].values() + temp_features['home'].values() + \
+                      [score_diff] + temp_features['game'].values() + [week_year]
         outputs.append(feature_lst)
-    #print default_timer() - iter_start
+
     header = ['away ' + x for x in temp_features['away']] + \
              ['home ' + x for x in temp_features['away']] + ['score diff', 'spread', 'roof', 'timeofgame', 'week_year']
     output_file = os.path.expanduser('~') + '/FSA/data/FeaturesByYear/features%d.csv' % year
