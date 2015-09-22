5f4a7a946b0ccdfd2a39167b002a9a646e572f81
edwardgarza <edward.garza0@gmail.com>
2015-09-21 21:59:28 -0400

    simulated possible returns with possible p vals

--- returns.py
+++ returns.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from numpy.random import normal
+from scipy.special import betainc
+import random as rand
 
 def returnsFromData(results, bet_percent, cons):
     '''Returns the final amount returned basaed on a given betting strategy.  
@@ -18,17 +21,18 @@ def returnsFromData(results, bet_percent, cons):
             (win_lst[-1] + loss_lst[-1])
     return bet
 
-def Percent(p, bet_percent, years, cons):
+def Percent(p, bet_percent, years, cons, output):
     ''' This is a simulation of what our returns would look like if we bet on all
     games each week, we bet evenly on all games, our returns are independent, and 
     we follow a strategy such that we bet a percent of our total investment each 
     week.  The histogram plot is of the log returns.  Values are final money for 
-    an investment of 1'''    
+    an investment of 1.  Output is a boolean that should be set to false, so 
+    that futureReturns can be used instead'''    
     # p = Success rate
     # bet_percent = amount of money to bet each week as a percent of total.  
     # number of simulations 
     # cons is a boolean that states if the betting percent is constant
-    simulations = 100000
+    simulations = 1000
     payoff = []
     final_payoff = np.array([])
     count = 0
@@ -50,6 +54,8 @@ def Percent(p, bet_percent, years, cons):
             payoff.append(bet)
         final_payoff = np.append(final_payoff, payoff[-1])
     final_payoff.sort()    
+    if not output:
+        return final_payoff
     # plotting the log of the payoff because it is gaussian
     plt.hist(np.log(final_payoff), int(simulations ** (1 / 3.0)))
     print '%1 worst', final_payoff[simulations / 100], 'mean', final_payoff.mean(), 'std', final_payoff.std()
@@ -90,3 +96,40 @@ def pEstGraph(wins, losses):
     plt.title('Estimation of p based on results')
     plt.plot(x, out)
     plt.show()
+
+def randP(wins, losses, size):
+    '''This function generates random values of p according to pdf of 
+    the MLE function. Size is the number of p values generated'''
+    x = np.linspace(0, 1, 1000)
+    # The integral of the pdf function from 0 to p is simply the incomplete beta
+    # function of wins+1, losses+1, p
+    cdf = [betainc(wins+1, losses+1, p) for p in x]
+    #plt.plot(x, cdf)
+    convert = {}
+    # create a dict so that we can invert this cdf
+    for elem in range(len(x)):
+        convert[x[elem]] = cdf[elem]
+    lst = []
+    for elem in range(size):
+        val = rand.random()
+        # finds the closest value to approximate an inversion function
+        key, value = min(convert.items(), key=lambda (_, v): abs(v - val))
+        lst.append(key)
+    return lst
+
+def futureReturns(wins, losses, bet_percent, cons):
+    '''gives a full estimation of the likely returns given some data.  Assumes
+    no change in week-week p.  This should be the function to use for simulating
+    returns.  I believe we should set cons to false.'''
+    # Generates p vals according to the MLE pdf
+    pvals = randP(wins, losses, 100)
+    returns = []
+    # for each of the random p vals generated, we run a simulation
+    for elem in pvals:
+        returns.append(Percent(elem, bet_percent, 1, cons, False))
+    # flatten the list of lists to a single list
+    returns = [item for sublist in returns for item in sublist]
+    return returns
+
+
+
